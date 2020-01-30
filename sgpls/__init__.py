73d819e43aab1640d94b109c7691b0687bcc9191
rs2518 <rs2518@ic.ac.uk>
2020-01-30 14:11:09 +0000

    Add __init__.py to sgpls/

---
+++ sgpls/__init__.py
@@ -0,0 +1,9 @@
+from ._pls import PLSCanonical, PLSRegression
+from ._spls import sPLSCanonical, sPLSRegression
+from ._gpls import gPLSCanonical, gPLSRegression
+from ._sgpls import sgPLSCanonical, sgPLSRegression
+
+__all__ = ['PLSCanonical', 'PLSRegression',
+           'sPLSCanonical', 'sPLSRegression',
+           'gPLSCanonical', 'gPLSRegression',
+           'sgPLSCanonical', 'sgPLSRegression']
