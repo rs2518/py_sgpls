0ab4076779232279cc393b1bb4bd190b7bf07696
rs2518 <rs2518@ic.ac.uk>
2020-03-18 20:07:27 +0000

    Add load_csv_data to read data from R

--- sgpls/tests/test_pls.py
+++ sgpls/tests/test_pls.py
@@ -10,6 +10,7 @@ sys.path.append(path)
 # =============================================================================
 # =============================================================================
 # import pytest
+import os
 
 from sgpls import _pls as pls_
 from sklearn.cross_decomposition import _pls as skpls_
@@ -18,14 +19,53 @@ import pandas as pd
 import numpy as np
 
 
-X_matrix = pd.read_csv(directory+'/data/simulated_data.csv',
-                       dtype='float').drop(columns=['Unnamed: 0'])
 
-Y_matrix = pd.read_csv(directory+'/data/simulated_target.csv',
-                       dtype='float').drop(columns=['Unnamed: 0'])
+# NOTE: There are Python packages that can run R code and handle R objects
+# however, neither worked in this case (-_-):
+#    - pyreadr parse method does not work on the models that were created 
+#    in R containing the information from the sgPLS implementation.
+#    
+#    - rpy2 cannot be imported because it requires an older version of Python.
 
-X = X_matrix.values
-Y = Y_matrix.values
+path = "Desktop/py_sgpls/data/"
+# folders = [f for f in os.listdir(path) if f.endswith("pls")]
+
+
+def load_csv_data(directory):
+    """Returns dictionary of data obtained from a directory
+    
+    Collects data loaded from all '.csv' files found in the given directory
+    into a dictionary. Data in the dictionary can be accessed using the
+    corresponding file name without the '.csv' extension.
+    """
+    dict_df = {}
+    for file in os.listdir(directory):
+        if file.endswith('.csv'):
+            df = pd.read_csv(os.path.join(directory, file), index_col=0)
+            # Drop strange column name
+            if len(df.columns)==1 and list(df.columns)==['get(model)[[item]]']:
+                df.rename(columns={'get(model)[[item]]': ''}, inplace=True)
+            dict_df[file[0:file.find('.csv')]] = df
+    
+    return dict_df
+
+
+sPLS_Rmodel = load_csv_data(os.path.join(path, 'sgPLS_spls'))
+gPLS_Rmodel = load_csv_data(os.path.join(path, 'sgPLS_gpls'))
+sgPLS_Rmodel = load_csv_data(os.path.join(path, 'sgPLS_sgpls'))
+
+
+
+# X_matrix = pd.read_csv(directory+'/data/simulated_data.csv',
+#                        dtype='float').drop(columns=['Unnamed: 0'])
+
+# Y_matrix = pd.read_csv(directory+'/data/simulated_target.csv',
+#                        dtype='float').drop(columns=['Unnamed: 0'])
+
+# X = X_matrix.values
+# Y = Y_matrix.values
+
+
 n_components = 3
 
 # =============================================================================
